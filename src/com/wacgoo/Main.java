7e8711531eb5487ced40605c8f0edb07e50e20ad
wacgoo <tatwaiho@gmail.com>
2015-04-21 22:21:47 -0700

    first commit

---
+++ src/com/wacgoo/Main.java
@@ -0,0 +1,8 @@
+package com.wacgoo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/wacgoo/alpha/AlphaClock.java
@@ -0,0 +1,7 @@
+package com.wacgoo.alpha;
+
+/**
+ * Created by tatwaiho on 4/8/15.
+ */
+public class AlphaClock {
+}
